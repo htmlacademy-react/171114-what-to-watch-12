2140338b6efc6d98166749b43407c52109f1566b
redcat <redcat-3@yandex.ru>
2023-03-30 22:03:23 +0800

    add interaction with the server

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,5 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
+import { Films } from '../types/film-info';
+import {AuthorizationStatus} from '../const';
 
 export const renderedFilmsInc = createAction('renderedFilmsInc');
 export const renderedFilmsReset = createAction('renderedFilmsReset');
 export const filmsCountSet = createAction<{filmsCount: number}>('filmCountSet');
+export const loadFilms = createAction<Films>('data/loadFilms');
+export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,26 +1,42 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { renderedFilmsInc, renderedFilmsReset, filmsCountSet } from './action';
-import { films } from '../mocks/film-info';
-import { FILM_COUNT_PER_STEP } from '../const';
+import { renderedFilmsInc, renderedFilmsReset, filmsCountSet, requireAuthorization, loadFilms } from './action';
+import { Films } from '../types/film-info';
+import { FILM_COUNT_PER_STEP, AuthorizationStatus } from '../const';
 
-const inisialState = {
-  renderedFilmsCount: Math.min(films.length, FILM_COUNT_PER_STEP),
-  filmsCount: films.length,
-  films
+type InitalState = {
+  renderedFilmsCount: number;
+  filmsCount: number;
+  films: Films;
+  authorizationStatus: AuthorizationStatus;
+}
+
+const inisialState: InitalState = {
+  films: [],
+  renderedFilmsCount: 0,
+  filmsCount: 0,
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 const reducer = createReducer(inisialState, (builder) => {
   builder
     .addCase(renderedFilmsInc, (state) => {
-      const filmCount = films.length;
+      const filmCount = state.films.length;
       const newRenderedFilmsCount = Math.min(filmCount, state.renderedFilmsCount + FILM_COUNT_PER_STEP);
       state.renderedFilmsCount = newRenderedFilmsCount;
     })
     .addCase(renderedFilmsReset, (state) => {
-      state.renderedFilmsCount = Math.min(films.length, FILM_COUNT_PER_STEP);
+      state.renderedFilmsCount = Math.min(state.films.length, FILM_COUNT_PER_STEP);
     })
     .addCase(filmsCountSet, (state, action) => {
       state.filmsCount = action.payload.filmsCount;
+    })
+    .addCase(loadFilms, (state, action) => {
+      state.films = action.payload;
+      state.filmsCount = state.films.length;
+      state.renderedFilmsCount = Math.min(state.films.length, FILM_COUNT_PER_STEP);
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
     });
 });
 
